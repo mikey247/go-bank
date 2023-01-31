7692ed1a27fc883713d1b08c06dbddd35a42db32
mikey247 <michaeljunior794@gmail.com>
2023-01-31 22:22:40 +0100

    commit

--- db/sqlc/store_test.go
+++ db/sqlc/store_test.go
@@ -36,7 +36,7 @@ func TestTransferTx(t *testing.T) {
 	}
 
 	// check results
-
+	existed := make(map[int]bool)
 	for i := 0; i < n; i++ {
 		err := <-errs
 		require.NoError(t, err)
@@ -77,7 +77,42 @@ func TestTransferTx(t *testing.T) {
 		_, err = store.GetEntry(context.Background(), toEntry.ID)
 		require.NoError(t, err)
 
-		// TODO: check account's balance
+		// check accounts
+		fromAccount := result.FromAccount
+		require.NotEmpty(t, fromAccount)
+		require.Equal(t, account1.ID, fromAccount.ID)
+
+		toAccount := result.ToAccount
+		require.NotEmpty(t, toAccount)
+		require.Equal(t, account2.ID, toAccount.ID)
+
+		// check account's balance
+		fmt.Println(">> tx:", fromAccount.Balance, toAccount.Balance)
+
+		diff1:= account1.Balance - fromAccount.Balance
+		diff2:= toAccount.Balance -  account2.Balance
+
+		require.Equal(t,diff1,diff2)
+		require.True(t, diff1>0)
+		require.True(t,diff1 %  amount == 0)
+
+		k:= int(diff1/amount)
+		require.True(t, k>= 1 && k<=n )
+		require.NotContains(t,existed,k)
+		existed[k]=true
+		
 	}
+ fmt.Println(existed)
+
+ updatedAccount1,err := testQueries.GetAccount(context.Background(), account1.ID)
+ require.NoError(t,err)
+
+ updatedAccount2,err := testQueries.GetAccount(context.Background(), account2.ID)
+ require.NoError(t,err)
+
+ fmt.Println(">> after:", updatedAccount1.Balance, updatedAccount2.Balance)
+
+	require.Equal(t, account1.Balance-int64(n)*amount, updatedAccount1.Balance)
+	require.Equal(t, account2.Balance+int64(n)*amount, updatedAccount2.Balance)
 
 }
