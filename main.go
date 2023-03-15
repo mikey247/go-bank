7cb8ec709df8f68ed01ff8e7385165cd3aff4099
mikey247 <michaeljunior794@gmail.com>
2023-03-15 10:42:16 +0100

    setup viper for config loading

--- main.go
+++ main.go
@@ -7,16 +7,21 @@ import (
 	_ "github.com/lib/pq"
 	"github.com/mikey247/go-bank/api"
 	bankdb "github.com/mikey247/go-bank/db/sqlc"
+	"github.com/mikey247/go-bank/util"
 )
 
-const (
-	dbDriver = "postgres"
-	dbSource = "postgresql://root:password@localhost:5432/simple_bank?sslmode=disable"
-	serverAddress = "0.0.0.0:5000"
-)
+// const (
+// 	dbDriver = "postgres"
+// 	dbSource = "postgresql://root:password@localhost:5432/simple_bank?sslmode=disable"
+// 	serverAddress = "0.0.0.0:5000"
+// )
 
 func main() {
-	conn,err := sql.Open(dbDriver, dbSource)
+	config,err:= util.LoadConfig(".")
+	if err != nil {
+		log.Fatal("cannot load config:", err)
+	}
+	conn,err := sql.Open(config.DBdriver, config.DBsource)
 
 	if err != nil {
 		log.Fatal("cannot connect to db:", err)
@@ -25,8 +30,19 @@ func main() {
 	store:= bankdb.NewStore(conn)
 	server:= api.NewServer(store)
 
-	err= server.Start(serverAddress)
+	err= server.Start(config.ServerAddress)
 	if err != nil{
 		log.Fatal("cannot start server:",err)
 	}
 }
+
+// func change (p *string){
+//  fmt.Println(*p,p)
+//  *p = "joey"
+// }
+
+// func main()  {
+// 	var name = "ross"
+// 	change(&name)
+// 	fmt.Println(name)
+// }
